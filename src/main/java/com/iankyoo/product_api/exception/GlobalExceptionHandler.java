5745b73e6e24f24092dc507f27ff66164d419eab
Iankyoo <contato.iankyoo@gmail.com>
2026-06-09 03:04:13 -0300

    feat: add Global Exception Handler

---
+++ src/main/java/com/iankyoo/product_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.iankyoo.product_api.exception;
+
+import com.iankyoo.product_api.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponse> productNotFoundHandler(ProductNotFoundException ex){
+        log.warn("Product not found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentsNotValid(MethodArgumentNotValidException ex) {
+        log.error("Method argument not valid");
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + "|" + error.getDefaultMessage())
+                .collect(Collectors.joining(","));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(message));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> genericExceptionHandler(Exception ex){
+        log.error("Unexpected error: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(ex.getMessage()));
+    }
+}
