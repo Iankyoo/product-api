b764a107e1f73ce5fdcbf37fe233ab9b02dad6c1
Iankyoo <contato.iankyoo@gmail.com>
2026-06-21 23:26:36 -0300

    feat: add RegisterRequest Dto

---
+++ src/main/java/com/iankyoo/product_api/dto/RegisterRequest.java
@@ -0,0 +1,16 @@
+package com.iankyoo.product_api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterRequest(
+        @NotBlank
+        String name,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String password
+
+) {
+}
