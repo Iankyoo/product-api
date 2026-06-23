247d9a938e727606250df2c339e581bbfe48b6cd
Iankyoo <contato.iankyoo@gmail.com>
2026-06-22 23:12:59 -0300

    feat: add JwtService

--- src/main/java/com/iankyoo/product_api/service/JwtService.java
+++ src/main/java/com/iankyoo/product_api/service/JwtService.java
@@ -1,4 +1,51 @@
 package com.iankyoo.product_api.service;
 
+import com.iankyoo.product_api.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private String expiration;
+
+    public SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateToken(User user){
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+    public String extractEmail(String token){
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token){
+        return extractClaims(token).getExpiration().after(new Date());
+    }
+
+    public Claims extractClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSecretKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 }
