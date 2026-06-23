e35d23e67bb97312a0ec1797c71c58e1c93650ee
Iankyoo <contato.iankyoo@gmail.com>
2026-06-22 23:27:24 -0300

    feat: add AuthService

---
+++ src/main/java/com/iankyoo/product_api/service/AuthService.java
@@ -0,0 +1,48 @@
+package com.iankyoo.product_api.service;
+
+import com.iankyoo.product_api.dto.AuthResponse;
+import com.iankyoo.product_api.dto.LoginRequest;
+import com.iankyoo.product_api.dto.RegisterRequest;
+import com.iankyoo.product_api.entity.Role;
+import com.iankyoo.product_api.entity.User;
+import com.iankyoo.product_api.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository repository;
+
+    public AuthService(JwtService jwtService, PasswordEncoder passwordEncoder, UserRepository repository){
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+        this.repository = repository;
+    }
+
+    public AuthResponse registerUser(RegisterRequest request){
+        User newUser = repository.save(User.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .role(Role.USER)
+                .build());
+
+        String token = jwtService.generateToken(newUser);
+        return new AuthResponse(token);
+    }
+
+    public AuthResponse login(LoginRequest request){
+        User user = repository.findByEmail(request.email())
+                .orElseThrow(() -> new RuntimeException("Invalid credentials"));
+
+        if (!passwordEncoder.matches(request.password(), user.getPassword())){
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        String token = jwtService.generateToken(user);
+        return new AuthResponse(token);
+    }
+}
