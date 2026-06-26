bdf75f8b91624267b46b33bb8271cdc9d3e98aed
Iankyoo <contato.iankyoo@gmail.com>
2026-06-26 01:53:59 -0300

    feat: add AuthController with 2 end-points

---
+++ src/main/java/com/iankyoo/product_api/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.iankyoo.product_api.controller;
+
+import com.iankyoo.product_api.dto.AuthResponse;
+import com.iankyoo.product_api.dto.LoginRequest;
+import com.iankyoo.product_api.dto.RegisterRequest;
+import com.iankyoo.product_api.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/auth")
+@RestController
+public class AuthController {
+    private final AuthService authService;
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(authService.registerUser(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+
+}
