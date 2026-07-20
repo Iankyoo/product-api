90886f7490b3ee9c736af4d8e213534b6c5c0bdc
Iankyoo <contato.iankyoo@gmail.com>
2026-07-19 23:03:56 -0300

    feat: Create Unit Test base

--- src/main/java/com/iankyoo/product_api/filters/JwtAuthFilter.java
+++ src/main/java/com/iankyoo/product_api/filters/JwtAuthFilter.java
@@ -26,6 +26,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         this.userRepository = userRepository;
     }
 
+    @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException{
---
+++ src/test/java/com/iankyoo/product_api/service/ProductServiceTest.java
@@ -0,0 +1,18 @@
+package com.iankyoo.product_api.service;
+
+import com.iankyoo.product_api.repository.ProductRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+}
